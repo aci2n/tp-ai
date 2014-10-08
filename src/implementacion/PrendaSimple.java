9c616da8a58a58b34c38256da082957ad753b772
Gabriel <Gabriel@Gabriel-PC>
2014-10-08 02:56:23 -0300

    -MORE

--- src/implementacion/PrendaSimple.java
+++ src/implementacion/PrendaSimple.java
@@ -5,4 +5,8 @@ import java.util.Collection;
 public abstract class PrendaSimple extends Prenda{
 	private Collection<ItemMaterial> materiales;
 	
+	public Collection<ItemMaterial> getMateriales() {
+		return this.materiales;
+	}
+	
 }
